3dd8bc05deddde72e901d195feccede1f6c26d62
Peterson Salamat <petersonsalamat@gmail.com>
2016-10-31 17:38:46 +0800

    Can now run sample test.

---
+++ Repo2.AcceptanceTests.Lib/FitNesse/start_server.bat
@@ -0,0 +1,2 @@
+C:\Users\Pete\GDrive\Apps\Common\CommonFiles\Java\bin\java.exe -jar fitnesse-standalone.jar -p 8090
+cmd /k
---
+++ Repo2.AcceptanceTests.Lib/ShouldIBuyMilk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo2.AcceptanceTests.Lib
+{
+    public class ShouldIBuyMilk //: fitSharp.Fit.Fixtures.
+    {
+        private int _cash;
+        private int _pintsOfMilkRemaining;
+        private string _useCreditCard;
+
+        public void SetCashInWallet(int cash)
+        {
+            _cash = cash;
+        }
+
+        public void SetCreditCard(string useCreditCard)
+        {
+            _useCreditCard = useCreditCard;
+        }
+
+        public void SetPintsOfMilkRemaining(int pints)
+        {
+            _pintsOfMilkRemaining = pints;
+        }
+
+        public string GoToStore()
+        {
+            if (_cash > 0 || _useCreditCard.Equals("yes"))
+                return "yes";
+            return "no";
+        }
+    }
+}
