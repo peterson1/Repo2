78aced97089914e1564ef637d325c308dda6dbf6
Peterson Salamat <petersonsalamat@gmail.com>
2016-12-10 17:21:20 +0800

    Added ViewModel extension method for setting DataContext and showing a window.

---
+++ Repo2.SDK.WPF45/Extensions/ViewModelExtensions/DataContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Repo2.SDK.WPF45.Extensions.ViewModelExtensions
+{
+    public static class DataContextExtensions
+    {
+        public static void ShowOn<T> (this object dataContext)
+            where T: Window, new()
+        {
+            var win = new T();
+            win.DataContext = dataContext;
+            win.Show();
+        }
+    }
+}
