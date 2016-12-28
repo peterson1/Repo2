8e2e845e21f7f9894f8668d65be88fb8447bd416
Peterson Salamat <petersonsalamat@gmail.com>
2016-12-29 04:04:43 +0800

    VisibleIfFalseConverter now converts value to nullable bool instead of a plain bool.

--- Repo2.SDK.WPF45/Converters/VisibleIfFalseConverter.cs
+++ Repo2.SDK.WPF45/Converters/VisibleIfFalseConverter.cs
@@ -8,7 +8,7 @@ namespace Repo2.SDK.WPF45.Converters
     public class VisibleIfFalseConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => (bool)value == false ? Visibility.Visible : Visibility.Collapsed;
+            => (bool?)value == false ? Visibility.Visible : Visibility.Collapsed;
 
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
