3c99606a5e2f9c8f7d9009c194ad87cd326be8c4
Peterson Salamat <petersonsalamat@gmail.com>
2017-01-29 05:18:48 +0800

    Added more date list generators.

--- Repo2.Core.ns11/DateTimeTools/DateTimeExtensions.cs
+++ Repo2.Core.ns11/DateTimeTools/DateTimeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Repo2.Core.ns11.DateTimeTools
 {
@@ -17,5 +18,36 @@ namespace Repo2.Core.ns11.DateTimeTools
                 currentDay = currentDay.AddDays(1);
             }
         }
+
+        public static IEnumerable<DateTime> EachDayUpToNow(this DateTime start)
+            => start.EachDayUpTo(DateTime.Now);
+
+
+
+        public static IEnumerable<DateTime> EachMonthUpTo(this DateTime start, DateTime end)
+            => start.EachDayUpTo       (end)
+                    .Select            (x => new DateTime(x.Year, x.Month, 1))
+                    .GroupBy           (x => x).Select(x => x.First())
+                    .OrderByDescending (x => x).ToList();
+
+        public static IEnumerable<DateTime> EachMonthUpToNow(this DateTime start)
+            => start.EachMonthUpTo(DateTime.Now);
+
+
+        public static IEnumerable<int> EachYearUpTo(this DateTime start, int endYear)
+        {
+            var yrs = new List<int>();
+            for (int i = endYear; i >= start.Year; i--)
+            {
+                yrs.Add(i);
+            }
+            return yrs;
+        }
+
+        public static IEnumerable<int> EachYearUpToNow(this DateTime start)
+            => start.EachYearUpTo(DateTime.Now.Year);
+
+        public static IEnumerable<int> EachYearUpToLastYear(this DateTime start)
+            => start.EachYearUpTo(DateTime.Now.Year - 1);
     }
 }
---
+++ Repo2.UnitTests.Lib/Core.ns11.Tests/DateTimeExtensions/DateTimeExtensionsFacts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Repo2.Core.ns11.DateTimeTools;
+using Xunit;
+
+namespace Repo2.UnitTests.Lib.Core.ns11.Tests.DateTimeExtensions
+{
+    [Trait("Core", "Unit")]
+    public class DateTimeExtensionsFacts
+    {
+        [Fact(DisplayName = "EachYearUpTo: 7 years")]
+        public void EachYearUpToTest1()
+        {
+            var start = new DateTime(2010, 5, 27);
+            var endYr = 2016;
+            var list  = start.EachYearUpTo(endYr);
+
+            list.Should().BeEquivalentTo(2016, 2015, 2014, 2013, 2012, 2011, 2010);
+            list.Should().BeInDescendingOrder();
+        }
+
+        [Fact(DisplayName = "EachYearUpTo: 1 year")]
+        public void EachYearUpTo1Yr()
+        {
+            var start = new DateTime(2010, 5, 27);
+            var endYr = 2010;
+            var list = start.EachYearUpTo(endYr);
+
+            list.Should().BeEquivalentTo(2010);
+        }
+    }
+}
