1d32fa705c5b7210b27a855b5c568edc30f249f4
Peterson Salamat <petersonsalamat@gmail.com>
2017-02-01 16:51:17 +0800

    Moved BesideExeCfgLoader from DSR to here.

---
+++ Repo2.Core.ns11/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo2.Core.ns11.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        //public static TVal GetOrDefault()
+    }
+}
---
+++ Repo2.SDK.WPF45/Configuration/BesideExeCfgLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Repo2.Core.ns11.FileSystems;
+
+namespace Repo2.SDK.WPF45.Configuration
+{
+    public class BesideExeCfgLoader<T> where T : class
+    {
+        const string SETTINGS_CFG = "settings.cfg";
+
+        private IFileSystemAccesor _fs;
+        private T _lastLoaded;
+
+        public BesideExeCfgLoader(IFileSystemAccesor fileSystemAccesor)
+        {
+            _fs = fileSystemAccesor;
+        }
+
+
+        public T Load(T defaultCfg)
+            => _lastLoaded ?? (_lastLoaded = ReadBesideExe(defaultCfg));
+
+
+        private T ReadBesideExe(T defaultCfg)
+        {
+            var p = _fs.GetBesideExeFilePath(SETTINGS_CFG);
+
+            try
+            {
+                return _fs.ReadJsonFileBesideExe<T>(SETTINGS_CFG);
+            }
+            catch (FileNotFoundException)
+            {
+                _fs.WriteJsonFileBesideExe(SETTINGS_CFG, defaultCfg);
+                return null;
+            }
+        }
+    }
+}
