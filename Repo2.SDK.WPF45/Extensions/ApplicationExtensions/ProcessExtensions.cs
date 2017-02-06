0575a82db77d049666dd147e73cffada2c9cfa25
Peterson Salamat <petersonsalamat@gmail.com>
2017-02-06 12:42:22 +0800

    Added App.Relaunch method to SDK.

---
+++ Repo2.SDK.WPF45/Extensions/ApplicationExtensions/ProcessExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using Repo2.Core.ns11.Extensions.StringExtensions;
+using Repo2.SDK.WPF45.FileSystems;
+
+namespace Repo2.SDK.WPF45.Extensions.ApplicationExtensions
+{
+    public static class ProcessExtensions
+    {
+        public static void Relaunch(this Application app, string arguments = null)
+        {
+            var origExe = new FileSystemAccesor1().CurrentExeFile;
+
+            if (arguments.IsBlank())
+                Process.Start(origExe);
+            else
+                Process.Start(origExe, arguments);
+
+            app.Shutdown();
+        }
+    }
+}
