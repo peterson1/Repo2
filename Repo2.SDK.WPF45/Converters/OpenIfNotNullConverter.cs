5bd8e5f95d764d1aa7a9d7e70d442da0e7c19993
Pete @BK1:SvrA <petersonsalamat@gmail.com>
2017-02-13 16:33:42 +0800

    Added a value converter for XTK, and a button style.

---
+++ Repo2.SDK.WPF45/Converters/OpenIfNotNullConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Xceed.Wpf.Toolkit;
+
+namespace Repo2.SDK.WPF45.Converters
+{
+    public class OpenIfNotNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => (value == null ? WindowState.Closed : WindowState.Open);
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
