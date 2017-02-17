fb252e1eefaa9382516e486a1e247e4810722299
Pete @BK1:SvrA <petersonsalamat@gmail.com>
2017-02-17 19:40:10 +0800

    OpenIfTrue converter for xtk:ChildWindow

---
+++ Repo2.SDK.WPF45/Converters/OpenIfTrueConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Xceed.Wpf.Toolkit;
+
+namespace Repo2.SDK.WPF45.Converters
+{
+    public class OpenIfTrueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => (bool)value ? WindowState.Open : WindowState.Closed;
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => (WindowState)value == WindowState.Open;
+    }
+}
