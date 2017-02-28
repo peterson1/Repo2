2400dd955ce6c3d92173c3f7f3859acc6b2c5993
Pete @BK1:SvrA <petersonsalamat@gmail.com>
2017-02-28 16:56:24 +0800

    Added publicly-raised SelectionChanged event to Observables.

--- Repo2.Core.ns11/DataStructures/Observables.cs
+++ Repo2.Core.ns11/DataStructures/Observables.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Repo2.Core.ns11.ChangeNotification;
 
 namespace Repo2.Core.ns11.DataStructures
 {
     public class Observables<T> : ObservableCollection<T>
     {
+        private      EventHandler _selectionChanged;
+        public event EventHandler  SelectionChanged
+        {
+            add    { _selectionChanged -= value; _selectionChanged += value; }
+            remove { _selectionChanged -= value; }
+        }
+
+
         public Observables() : base()
         {
         }
@@ -24,5 +34,9 @@ namespace Repo2.Core.ns11.DataStructures
             foreach (var item in newItems)
                 this.Add(item);
         }
+
+
+        public void RaiseSelectionChanged()
+            => _selectionChanged.Raise();
     }
 }
