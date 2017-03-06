0d735951188df0ba060627daa305b73ad5f03d7e
Pete @BK1:SvrA <petersonsalamat@gmail.com>
2017-03-06 09:14:12 +0800

    Started working on auto-recovery for MariaDB, but decided to delay it because the newly-added Logoff script might be enough to keep the DB from crashing.

---
+++ Repo2.SDK.WPF45/Databases/MariaDbRecovery.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Repo2.Core.ns11.Exceptions;
+
+namespace Repo2.SDK.WPF45.Databases
+{
+    public class MariaDbRecovery
+    {
+        const string FLAG_KEY   = "innodb_force_recovery";
+        const string MY_INI_URI = @"core\mysql\my.ini";
+        const string START_BAT  = @"scripts\start_mysql.bat";
+        const string STOP_BAT   = @"scripts\stop_mysql.bat";
+
+
+        public static async Task ForceRecover(string uniserverBaseDir)
+        {
+            var iniPath = FindMyIniFile(uniserverBaseDir);
+            SetForceRecoveryFlag(1, iniPath);
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+
+
+        private static string FindMyIniFile(string uniserverBaseDir)
+        {
+            var iniPath = Path.Combine(uniserverBaseDir, MY_INI_URI);
+
+            if (!File.Exists(iniPath))
+                throw Fault.Missing("MariaDB ini file", iniPath);
+
+            return iniPath;
+        }
+
+
+        private static void SetForceRecoveryFlag(int forceRecoveryCode, string iniPath)
+        {
+            var lines = File.ReadAllLines(iniPath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Trim().StartsWith(FLAG_KEY))
+                    lines[i] = $"{FLAG_KEY} = {forceRecoveryCode}";
+            }
+
+            File.WriteAllLines(iniPath, lines);
+        }
+    }
+}
