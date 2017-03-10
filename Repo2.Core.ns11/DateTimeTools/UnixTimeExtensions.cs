5e1b4f578933e4b9832a7911a5e49afc7b97b248
Pete @BK1:SvrA <petersonsalamat@gmail.com>
2017-03-10 17:34:16 +0800

    Added UnixTimeExtensions for EpochDate.

---
+++ Repo2.Core.ns11/DateTimeTools/UnixTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repo2.Core.ns11.DateTimeTools
+{
+    public static class UnixTimeExtensions
+    {
+        static DateTime EpochDate => new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+
+        public static DateTime ToDateFromUnixSeconds(this double unixTimeSeconds)
+            => EpochDate.AddSeconds(unixTimeSeconds).ToLocalTime();
+    }
+}
