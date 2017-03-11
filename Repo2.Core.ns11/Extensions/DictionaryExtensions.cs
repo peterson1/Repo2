cb1ffc5d4d5de7c90c328cb57c4fb0e03e45a472
Peterson Salamat <petersonsalamat@gmail.com>
2017-03-12 05:40:20 +0800

    Added Dictionary extension GetOrDefault from a SO answer by Jon Skeet.

--- Repo2.Core.ns11/Extensions/DictionaryExtensions.cs
+++ Repo2.Core.ns11/Extensions/DictionaryExtensions.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repo2.Core.ns11.Extensions
 {
+    //http://stackoverflow.com/a/2601501/3973863
     public static class DictionaryExtensions
     {
-        //public static TVal GetOrDefault()
+        public static TValue GetOrDefault<TKey, TValue>
+            (this IDictionary<TKey, TValue> dictionary,
+             TKey key,
+             TValue defaultValue = default(TValue))
+        {
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+        }
+
+        public static TValue GetOrDefault<TKey, TValue>
+            (this IDictionary<TKey, TValue> dictionary,
+             TKey key,
+             Func<TValue> defaultValueProvider)
+        {
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value
+                 : defaultValueProvider();
+        }
     }
 }
