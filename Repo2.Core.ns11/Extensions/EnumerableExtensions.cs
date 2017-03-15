18e0991144bf7bb7259feddae58167feef80033f
Peterson Salamat <petersonsalamat@gmail.com>
2017-03-16 03:37:11 +0800

    Tried to add ToDictionary collection extension, but discovered that Linq had it already.

---
+++ Repo2.Core.ns11/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repo2.Core.ns11.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        //public static Dictionary<TKey, TVal> ToDictionary<TKey, TVal>(this IEnumerable<TVal> colxn, Func<TVal, TKey> keySelector)
+        //{
+        //    var dict = new Dictionary<TKey, TVal>();
+
+        //    foreach (var item in colxn)
+        //        dict.Add(keySelector(item), item);
+
+        //    return dict;
+        //}
+    }
+}
