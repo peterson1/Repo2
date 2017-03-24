3fad97d8bfe1785798fceba914bf59f1d48b7392
Pete @BK1:SvrA <petersonsalamat@gmail.com>
2017-03-24 13:51:06 +0800

    Added DnsUpdater project.

---
+++ Repo2.DnsUpdater.WPF/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+
+namespace Repo2.DnsUpdater.WPF
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var hostEntry = Dns.GetHostEntry("google.com");
+
+
+            foreach (var addr in hostEntry.AddressList)
+            {
+                Console.WriteLine(addr.ToString());
+            }
+        }
+    }
+}
