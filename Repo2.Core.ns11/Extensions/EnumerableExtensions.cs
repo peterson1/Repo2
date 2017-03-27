91abeb3aad11825e92b01f9eeba0587e0472010d
Pete @BK1:SvrA <petersonsalamat@gmail.com>
2017-03-27 09:42:56 +0800

    Overloaded Linq's SingleOrDefault method to show a more helpful error message when finding duplicates.

--- Repo2.Core.ns11/Extensions/EnumerableExtensions.cs
+++ Repo2.Core.ns11/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using Repo2.Core.ns11.Exceptions;
 
 namespace Repo2.Core.ns11.Extensions
 {
@@ -14,5 +16,16 @@ namespace Repo2.Core.ns11.Extensions
 
         //    return dict;
         //}
+
+        public static T SingleOrDefault<T>(this IEnumerable<T> items,
+            Func<T, bool> predicate, string key, object value, string operatr = "==")
+        {
+            var matches = items.Where(predicate);
+
+            if (matches.Count() == 0) return default(T);
+            if (matches.Count() == 1) return matches.First();
+
+            throw Fault.MultiMatch<T>(key, value, operatr);
+        }
     }
 }
