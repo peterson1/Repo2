169b24dbe5f9f6082811cf6db710fd6b7ad88a56
Pete @BK1:SvrA <petersonsalamat@gmail.com>
2017-04-21 14:37:40 +0800

    Added IsBusy and Status setter methods to R2ViewModelBase.

--- Repo2.SDK.WPF45/ViewModelTools/R2ViewModelBase.cs
+++ Repo2.SDK.WPF45/ViewModelTools/R2ViewModelBase.cs
@@ -9,6 +9,13 @@ namespace Repo2.SDK.WPF45.ViewModelTools
     [ImplementPropertyChanged]
     public class R2ViewModelBase : INotifyPropertyChanged
     {
+        private      EventHandler<string> _statusChanged;
+        public event EventHandler<string>  StatusChanged
+        {
+            add    { _statusChanged -= value; _statusChanged += value; }
+            remove { _statusChanged -= value; }
+        }
+
         protected    PropertyChangedEventHandler _propertyChanged;
         public event PropertyChangedEventHandler  PropertyChanged
         {
@@ -32,10 +39,26 @@ namespace Repo2.SDK.WPF45.ViewModelTools
         }
 
 
-        public string Title { get; private set; }
-
+        public string   Title     { get; private set; }
+        public bool     IsBusy    { get; private set; }
+        public string   BusyText  { get; private set; }
+        public string   Status    { get; private set; }
 
         protected void UpdateTitle(string text) => Title = text;
+
+
+        protected void SetStatus(string status)
+            => _statusChanged?.Raise(Status = status);
+
+
+        protected void StartBeingBusy(string message)
+        {
+            IsBusy = true;
+            BusyText = message;
+        }
+
+        protected void StopBeingBusy() => IsBusy = false;
+
 
 
         public void Activate() 
