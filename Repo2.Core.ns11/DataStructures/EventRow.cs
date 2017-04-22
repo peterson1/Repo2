dc7f3af1c64ec9f3f6ff68f11c638c90eef50f36
Peterson Salamat <petersonsalamat@gmail.com>
2017-04-22 20:06:43 +0800

    Added initial data structures for supporting Event Sourcing.

---
+++ Repo2.Core.ns11/DataStructures/EventRow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repo2.Core.ns11.DataStructures
+{
+    public class EventRow
+    {
+        public DateTime  Timestamp   { get; set; }
+        public int       UserID      { get; set; }
+        public string    FieldName   { get; set; }
+        public object    FieldValue  { get; set; }
+    }
+}
