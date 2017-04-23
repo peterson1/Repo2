f367ac70ea686ca6d259fc4b76e49becb7ea4bea
Peterson Salamat <petersonsalamat@gmail.com>
2017-04-24 03:52:47 +0800

    Added static thread helper class for running actions/tasks in a new thread.

---
+++ Repo2.Core.ns11/Threads/NewThread.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo2.Core.ns11.Threads
+{
+    public class NewThread
+    {
+        public static async Task Run(Action action)
+        {
+            await Task.Run(async () =>
+            {
+                await Task.Delay(1);
+                action.Invoke();
+            }
+            ).ConfigureAwait(false);
+        }
+
+
+        public static async Task<T> Run<T>(Func<T> task)
+        {
+            return await Task.Run(async () =>
+            {
+                await Task.Delay(1);
+                return task.Invoke();
+            }
+            ).ConfigureAwait(false);
+        }
+    }
+}
