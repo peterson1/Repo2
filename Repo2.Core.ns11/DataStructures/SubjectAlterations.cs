b92fb79f798b4b4d4a51e9c0251f8571152a7294
Peterson Salamat <petersonsalamat@gmail.com>
2017-04-24 03:48:21 +0800

    Renamed event sourcing data structures to be more descriptive.

---
+++ Repo2.Core.ns11/DataStructures/SubjectAlterations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repo2.Core.ns11.DataStructures
+{
+    public class SubjectAlterations : List<SubjectValueMod>
+    {
+        public SubjectAlterations(int userId, int subjectId)
+        {
+            UserID    = userId;
+            SubjectID = subjectId;
+        }
+
+
+        public void Add (string fieldName, object newValue)
+        {
+            Add(new SubjectValueMod
+            {
+                Timestamp  = DateTime.Now,
+                UserID     = this.UserID,
+                SubjectID  = this.SubjectID,
+                FieldName  = fieldName,
+                NewValue   = newValue
+            });
+        }
+
+
+        public int  UserID     { get; }
+        public int  SubjectID  { get; }
+    }
+}
--- Repo2.Core.ns11/DataStructures/SubjectValueMod.cs
+++ Repo2.Core.ns11/DataStructures/SubjectValueMod.cs
@@ -2,11 +2,12 @@
 
 namespace Repo2.Core.ns11.DataStructures
 {
-    public class EventRow
+    public class SubjectValueMod
     {
         public DateTime  Timestamp   { get; set; }
         public int       UserID      { get; set; }
+        public int       SubjectID   { get; set; }
         public string    FieldName   { get; set; }
-        public object    FieldValue  { get; set; }
+        public object    NewValue    { get; set; }
     }
 }
