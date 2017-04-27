ee68b55ffc04a87fdd64a4d0aefacb22974d20d9
Peterson Salamat <petersonsalamat@gmail.com>
2017-04-27 17:39:14 +0800

    Added a base class containing common methods for snapshot models.

---
+++ Repo2.Core.ns11/DataStructures/SubjectSnapshotBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Repo2.Core.ns11.DataStructures
+{
+    public abstract class SubjectSnapshotBase : ISubjectSnapshot
+    {
+        public abstract int SubjectId { get; }
+
+        public abstract void ApplyAlterations(IEnumerable<SubjectValueMod> allMods);
+
+
+        protected T Last<T>(string fieldName, List<IGrouping<string, SubjectValueMod>> grpd)
+        {
+            var fGrp = grpd.SingleOrDefault(x => x.Key == fieldName);
+            if (fGrp == null) return default(T);
+            return (T)fGrp.Last().NewValue;
+        }
+    }
+}
