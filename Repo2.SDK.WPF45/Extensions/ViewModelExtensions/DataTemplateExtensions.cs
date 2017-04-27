47fd199d2324b83dfec5b3adc71dbe85fca134a8
Peterson Salamat <petersonsalamat@gmail.com>
2017-04-27 15:37:28 +0800

    Supported accepting types as parameters for SetTemplates.

--- Repo2.SDK.WPF45/Extensions/ViewModelExtensions/DataTemplateExtensions.cs
+++ Repo2.SDK.WPF45/Extensions/ViewModelExtensions/DataTemplateExtensions.cs
@@ -1,14 +1,25 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Repo2.SDK.WPF45.Extensions.ViewModelExtensions
 {
     public static class DataTemplateExtensions
     {
         public static void SetTemplate<TData, TUiElement>(this Application app)
+            => app.SetTemplate(typeof(TData), typeof(TUiElement));
+        //{
+        //    var dt = new DataTemplate(typeof(TData));
+        //    dt.VisualTree = new FrameworkElementFactory(typeof(TUiElement));
+        //    var key = new DataTemplateKey(typeof(TData));
+        //    app.Resources.Add(key, dt);
+        //}
+
+
+        public static void SetTemplate(this Application app, Type dataType, Type uiElementType)
         {
-            var dt = new DataTemplate(typeof(TData));
-            dt.VisualTree = new FrameworkElementFactory(typeof(TUiElement));
-            var key = new DataTemplateKey(typeof(TData));
+            var dt = new DataTemplate(dataType);
+            dt.VisualTree = new FrameworkElementFactory(uiElementType);
+            var key = new DataTemplateKey(dataType);
             app.Resources.Add(key, dt);
         }
     }
