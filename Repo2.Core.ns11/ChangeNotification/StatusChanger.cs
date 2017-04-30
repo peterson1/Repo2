09a631c20095926bf350e08a3f74dc4f29e9d32e
Peterson Salamat <petersonsalamat@gmail.com>
2017-04-30 11:57:30 +0800

    Added new implementation of StatusChanger which prevents firing the same event handler twice.

--- Repo2.Core.ns11/ChangeNotification/StatusChanger.cs
+++ Repo2.Core.ns11/ChangeNotification/StatusChanger.cs
@@ -1,22 +1,36 @@
 ﻿using System;
+using System.Linq;
 
 namespace Repo2.Core.ns11.ChangeNotification
 {
     public class StatusChanger : IStatusChanger
     {
-        private      EventHandler<string> _statusChanged;
+        protected      EventHandler<string> _statusChanged;
         public event EventHandler<string>  StatusChanged
         {
-            add    { _statusChanged -= value; _statusChanged += value; }
+            //add    { _statusChanged -= value; _statusChanged += value; }
+            add
+            {
+                //if (_statusChanged == null || !_statusChanged.GetInvocationList().Contains(value))
+                TryRemoveExisting(value);
+                _statusChanged += value;
+            }
             remove { _statusChanged -= value; }
         }
-
 
         public string Status { get; private set; }
 
 
+        protected virtual void TryRemoveExisting(EventHandler<string> value)
+        {
+            _statusChanged -= value;
+        }
+
 
         protected virtual void SetStatus(string statusText)
             => _statusChanged?.Raise(Status = statusText);
+
+
+        //public int InvocatorsCount => _statusChanged?.GetInvocationList()?.Count() ?? 0;
     }
 }
---
+++ Repo2.SDK.WPF45/ChangeNotification/StatusChangerN45.cs
@@ -0,0 +1,30 @@
+﻿using Repo2.Core.ns11.ChangeNotification;
+using System;
+
+namespace Repo2.SDK.WPF45.ChangeNotification
+{
+    public class StatusChangerN45 : StatusChanger
+    {
+        protected override void TryRemoveExisting(EventHandler<string> newHandler)
+        {
+            if (_statusChanged == null) return;
+            var newKey = GetKey(newHandler);
+            foreach (var @delegate in _statusChanged.GetInvocationList())
+            {
+                var oldHandlr = @delegate as EventHandler<string>;
+                if (GetKey(oldHandlr) == newKey)
+                {
+                    _statusChanged -= oldHandlr;
+                    return;
+                }
+            }
+        }
+
+
+        private string GetKey(EventHandler<string> handlr)
+        {
+            var typ = handlr.Method.DeclaringType.FullName;
+            return $"{typ}.{handlr.Method.Name}";
+        }
+    }
+}
