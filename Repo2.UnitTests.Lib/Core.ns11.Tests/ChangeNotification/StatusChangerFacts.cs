59f1bfb5dcb8c63daf722f5caa21559937fd3add
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-05-05 10:49:53 +0800

    Added unit test for preventing event-multicast in StatusChanger.

---
+++ Repo2.UnitTests.Lib/Core.ns11.Tests/ChangeNotification/StatusChangerFacts.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Repo2.Core.ns11.ChangeNotification;
+using Repo2.SDK.WPF45.ChangeNotification;
+using Xunit;
+
+namespace Repo2.UnitTests.Lib.Core.ns11.Tests.ChangeNotification
+{
+    [Trait("Core", "Unit")]
+    public class StatusChangerFacts
+    {
+        [Fact(DisplayName = "StatusChanged won't fire twice", Skip = "undone")]
+        public void StatusChangedWontFireTwice()
+        {
+            var changr = new TestChanger();
+            var countr = new TestCounter();
+
+            var testC1 = new TestClass(changr, countr);
+            var testC2 = new TestClass(changr, countr);
+
+            changr.RaiseStatusChange();
+
+            countr.CallCount.Should().Be(1);
+        }
+
+
+        class TestChanger : StatusChanger
+        {
+            public void RaiseStatusChange()
+                => SetStatus("abc");
+        }
+
+
+        class TestClass
+        {
+            public TestClass(StatusChanger changr, TestCounter countr)
+            {
+                changr.StatusChanged += (s, e) => countr.CallCount++;
+            }
+        }
+
+
+        class TestCounter
+        {
+            public int CallCount { get; set; }
+        }
+    }
+}
---
+++ Repo2.UnitTests.Lib/SDK.WPF45.Tests/ChangeNotification/StatusChangerN45Facts.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Repo2.SDK.WPF45.ChangeNotification;
+using Xunit;
+
+namespace Repo2.UnitTests.Lib.SDK.WPF45.Tests.ChangeNotification
+{
+    [Trait("Core", "Unit")]
+    public class StatusChangerN45Facts
+    {
+        [Fact(DisplayName = "StatusChanged won't fire twice")]
+        public void StatusChangedWontFireTwice()
+        {
+            var changr = new TestChanger();
+            var countr = new TestCounter();
+
+            var testC1 = new TestClass(changr, countr);
+            var testC2 = new TestClass(changr, countr);
+
+            changr.RaiseStatusChange();
+
+            countr.CallCount.Should().Be(1);
+        }
+
+
+        class TestChanger : StatusChangerN45
+        {
+            public void RaiseStatusChange()
+                => SetStatus("abc");
+        }
+
+
+        class TestClass
+        {
+            public TestClass(StatusChangerN45 changr, TestCounter countr)
+            {
+                changr.StatusChanged += (s, e) => countr.CallCount++;
+            }
+        }
+
+
+        class TestCounter
+        {
+            public int CallCount { get; set; }
+        }
+    }
+}
