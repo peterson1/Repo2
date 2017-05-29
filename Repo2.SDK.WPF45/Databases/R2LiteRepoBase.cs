4e9183a08cc3b158a8dcef30a4c86ef8786bafbc
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-05-29 17:45:14 +0800

    Abstracted FindAll method.

--- Repo2.SDK.WPF45/Databases/R2LiteRepoBase.cs
+++ Repo2.SDK.WPF45/Databases/R2LiteRepoBase.cs
@@ -3,7 +3,9 @@ using Repo2.Core.ns11.Databases;
 using Repo2.Core.ns11.Extensions.StringExtensions;
 using Repo2.Core.ns11.FileSystems;
 using Repo2.SDK.WPF45.ChangeNotification;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Repo2.SDK.WPF45.Databases
 {
@@ -65,6 +67,19 @@ namespace Repo2.SDK.WPF45.Databases
             var db     = CreateLiteDatabase();
             collection = db.GetCollection<T>(collectionName ?? typeof(T).Name);
             return db;
+        }
+
+
+        public virtual List<T> FindAll<T>()
+        {
+            List<T> list;
+            SetStatus($"Querying ALL ‹{typeof(T).Name}› records ...");
+
+            using (var db = ConnectToDB(out LiteCollection<T> col))
+                list = col.FindAll().ToList();
+
+            SetStatus($"Query returned {list.Count:N0} ‹{typeof(T).Name}› records.");
+            return list;
         }
     }
 }
