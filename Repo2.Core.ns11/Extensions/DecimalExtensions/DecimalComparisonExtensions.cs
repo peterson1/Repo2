359302d875a71c824f1bbc19975f90da565a2ca4
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-05-30 11:53:26 +0800

    Added fuzzy comparer for decimals and doubles.

---
+++ Repo2.Core.ns11/Extensions/DecimalExtensions/DecimalComparisonExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Repo2.Core.ns11.Extensions.DecimalExtensions
+{
+    public static class DecimalComparisonExtensions
+    {
+        public static bool AlmostEqualTo(this decimal value1, decimal value2, decimal precision = 0.0000001M)
+            => Math.Abs(value1 - value2) < precision;
+
+
+        public static bool AlmostEqualTo(this double value1, double value2, decimal precision = 0.0000001M)
+            => ((decimal)value1).AlmostEqualTo((decimal)value2, precision);
+
+
+        public static bool AlmostEqualTo(this double value1, decimal value2, decimal precision = 0.0000001M)
+            => ((decimal)value1).AlmostEqualTo(value2, precision);
+
+
+        public static bool AlmostEqualTo(this decimal value1, double value2, decimal precision = 0.0000001M)
+            => (value1).AlmostEqualTo((decimal)value2, precision);
+    }
+}
