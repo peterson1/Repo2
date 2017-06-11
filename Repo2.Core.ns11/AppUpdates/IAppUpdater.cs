0443ac51f6e2ce91e3bc50e1e8ba12e7fda9d85d
Peterson Salamat <petersonsalamat@gmail.com>
2017-06-11 19:39:04 +0800

    Added minimal AppUpdater interface.

---
+++ Repo2.Core.ns11/AppUpdates/IAppUpdater.cs
@@ -0,0 +1,12 @@
+﻿using Repo2.Core.ns11.ChangeNotification;
+using System;
+
+namespace Repo2.Core.ns11.AppUpdates
+{
+    public interface IAppUpdater : IStatusChanger
+    {
+        event EventHandler UpdatesInstalled;
+
+        void StartCheckingForUpdates();
+    }
+}
