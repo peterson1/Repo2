6b308f934fdd8bcf2800ad4db472c44bab94c505
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-06-14 11:41:25 +0800

    Added reusable UI for R2AppUpdater.

---
+++ Repo2.SDK.WPF45/AppUpdates/R2AppUpdaterUI1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Repo2.SDK.WPF45.AppUpdates
+{
+    /// <summary>
+    /// Interaction logic for R2AppUpdaterUI1.xaml
+    /// </summary>
+    public partial class R2AppUpdaterUI1 : UserControl
+    {
+        public R2AppUpdaterUI1()
+        {
+            InitializeComponent();
+        }
+    }
+}
