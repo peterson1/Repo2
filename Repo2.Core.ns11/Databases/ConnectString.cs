2b5f9dba5ef5fff2fff4c0dcd7f042e95fbdafa6
Peterson Salamat <petersonsalamat@gmail.com>
2017-06-21 10:37:14 +0800

    Added ConnectString builder for MS Access.

--- Repo2.Core.ns11/Databases/ConnectString.cs
+++ Repo2.Core.ns11/Databases/ConnectString.cs
@@ -1,8 +1,22 @@
-﻿namespace Repo2.Core.ns11.Databases
+﻿using Repo2.Core.ns11.Extensions.StringExtensions;
+
+namespace Repo2.Core.ns11.Databases
 {
     public class ConnectString
     {
         public static string LiteDB(string filePath, string mode = "Exclusive", bool journal = false)
             => $"Filename={filePath};Mode={mode};Journal={journal};";
+
+
+        public static string MsAccess(string filePath, string password = null)
+        {
+            var cs = $"Provider=Microsoft.Jet.OLEDB.4.0;"
+                   + $"Data Source={filePath};";
+
+            if (!password.IsBlank())
+                cs += $"Jet OLEDB:Database Password={password};";
+
+            return cs;
+        }
     }
 }
