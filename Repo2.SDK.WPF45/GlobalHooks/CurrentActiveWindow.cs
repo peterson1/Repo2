cfa4e2a84318851f42d057c7eb916c3616e9d09c
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-07-11 17:06:44 +0800

    Added method from SO

---
+++ Repo2.SDK.WPF45/GlobalHooks/CurrentActiveWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Repo2.SDK.WPF45.GlobalHooks
+{
+    //https://stackoverflow.com/a/115905/3973863
+    public class CurrentActiveWindow
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+
+        public static string GetTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+    }
+}
