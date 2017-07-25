fc2f25297cf7eb1ef3a16fab3d51e6bc4cb7ca31
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-07-25 10:06:33 +0800

    Added KeyPressLog DTO

---
+++ Repo2.SDK.WPF45/GlobalHooks/KeyPressLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace Repo2.SDK.WPF45.GlobalHooks
+{
+    public class KeyPressLog
+    {
+        public List<LogEntry>  Entries  { get; set; }
+
+
+        public class LogEntry
+        {
+            public DateTime  Timestamp    { get; set; }
+            public Key       KeyPressed   { get; set; }
+            public string    KeyText      { get; set; }
+            public string    WindowTitle  { get; set; }
+        }
+
+
+        public void Add(Key key, string windowTitle = null)
+        {
+            if (Entries == null)
+                Entries = new List<LogEntry>();
+
+            Entries.Add(new LogEntry
+            {
+                Timestamp   = DateTime.Now,
+                KeyPressed  = key,
+                KeyText     = key.ToString(),
+                WindowTitle = windowTitle,
+            });
+        }
+    }
+}
