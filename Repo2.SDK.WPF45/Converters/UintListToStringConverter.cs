2c012fbf4f6b621816b25a7a77d3582a445462e5
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-07-27 10:33:29 +0800

    added new converter

---
+++ Repo2.SDK.WPF45/Converters/UintListToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Repo2.SDK.WPF45.Converters
+{
+    [ValueConversion(typeof(List<uint>), typeof(string))]
+    public class UintListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(string))
+                throw new InvalidOperationException("The target must be a String");
+
+            return String.Join(", ", ((List<uint>)value).ToArray());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
